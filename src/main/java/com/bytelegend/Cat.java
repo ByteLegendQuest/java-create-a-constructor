4abc2a14baaabf29dabaab4a8a55747d851a3a9b
michealmachine <micheal0machine@gmail.com>
2022-04-06 07:44:39 +0000

    Update at 2022-04-06T07:44:39.513308Z

--- src/main/java/com/bytelegend/Cat.java
+++ src/main/java/com/bytelegend/Cat.java
@@ -2,8 +2,9 @@ package com.bytelegend;
 
 public class Cat {
     public String name;
-    Cat(){
-        
+    Cat(String name){
+        this.name=name;
+
     }
     public static void main(String[] args) {
         System.out.println(new Cat("Black"));
