825884ec409de4a41ea4919891d20ecfc1d09f73
lkskjjk <yun.11@yandex.com>
2022-05-05 01:49:44 +0000

    Update at 2022-05-05T01:49:44.425547Z

--- src/main/java/com/bytelegend/Cat.java
+++ src/main/java/com/bytelegend/Cat.java
@@ -3,6 +3,12 @@ package com.bytelegend;
 public class Cat {
     public String name;
 
+    private void Cat(name){
+        Cat cat = new Cat();
+        cat.name =name;
+        return cat;
+    }
+
     public static void main(String[] args) {
         System.out.println(new Cat("Black"));
         System.out.println(new Cat("White"));
