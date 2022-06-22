3848be7e63a236d089316a76468d6670ed91eacc
batman405 <2638603343@qq.com>
2022-06-22 09:10:55 +0000

    Update at 2022-06-22T09:10:55.245341Z

--- src/main/java/com/bytelegend/Cat.java
+++ src/main/java/com/bytelegend/Cat.java
@@ -3,6 +3,8 @@ package com.bytelegend;
 public class Cat {
     public String name;
 
+    Cat(String name){}
+
     public static void main(String[] args) {
         System.out.println(new Cat("Black"));
         System.out.println(new Cat("White"));
