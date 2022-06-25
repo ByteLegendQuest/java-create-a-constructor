9d9fb2f879c774d1048712346211bb56857a1c56
Anlyui <1391469813@qq.com>
2022-06-25 10:51:42 +0000

    Update at 2022-06-25T10:51:42.201483Z

--- src/main/java/com/bytelegend/Cat.java
+++ src/main/java/com/bytelegend/Cat.java
@@ -8,6 +8,8 @@ public class Cat {
         System.out.println(new Cat("White"));
     }
 
+    public Cat(String name) {}
+
     @Override
     public String toString() {
         return "Cat(" + name + ")";
