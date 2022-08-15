7c66146332fe72004dd59633c4da02176d9da6e6
Super-XiaoNian <1962971038@qq.com>
2022-08-15 00:24:16 +0000

    Update at 2022-08-15T00:24:16.103209Z

--- src/main/java/com/bytelegend/Cat.java
+++ src/main/java/com/bytelegend/Cat.java
@@ -3,15 +3,11 @@ package com.bytelegend;
 public class Cat {
     public String name;
 
+    public Cat(String name) {}
 
     public static void main(String[] args) {
         System.out.println(new Cat("Black"));
         System.out.println(new Cat("White"));
-    }
-
-
-    public Cat(String name) {
-        
     }
 
     @Override
