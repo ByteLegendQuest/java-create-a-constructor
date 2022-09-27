07cfa449504ea2b76d4218c0e98ae8174ba7ca81
Chen-anP <1280707621@qq.com>
2022-09-27 13:22:18 +0000

    Update at 2022-09-27T13:22:18.489254Z

--- src/main/java/com/bytelegend/Cat.java
+++ src/main/java/com/bytelegend/Cat.java
@@ -3,8 +3,8 @@ package com.bytelegend;
 public class Cat {
     public String name;
 
-    public Cat (String name) {
-
+    public Cat(String name) {
+        this.name = name;
     }
 
     public static void main(String[] args) {
