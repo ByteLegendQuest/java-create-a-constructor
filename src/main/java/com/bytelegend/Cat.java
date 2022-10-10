fd54e67c73dad383e6762d6f71fc151493be1aa8
Mooos-MoSheng <1297393910@qq.com>
2022-10-10 07:19:31 +0000

    Update at 2022-10-10T07:19:31.485526Z

--- src/main/java/com/bytelegend/Cat.java
+++ src/main/java/com/bytelegend/Cat.java
@@ -3,6 +3,12 @@ package com.bytelegend;
 public class Cat {
     public String name;
 
+    public static String Cat(String s)
+    {
+        name = s;
+        return toString(name);
+    }
+
     public static void main(String[] args) {
         System.out.println(new Cat("Black"));
         System.out.println(new Cat("White"));
