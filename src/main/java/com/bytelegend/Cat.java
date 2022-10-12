8c68b1c2fbc9337463d958fedbe2bbb2b6c81542
tiger0961 <29515547@qq.com>
2022-10-12 02:01:54 +0000

    Update at 2022-10-12T02:01:54.664285Z

--- src/main/java/com/bytelegend/Cat.java
+++ src/main/java/com/bytelegend/Cat.java
@@ -4,8 +4,8 @@ public class Cat {
     public String name;
 
     public static void main(String[] args) {
-        System.out.println(new Cat("Black"));
-        System.out.println(new Cat("White"));
+        System.out.println((new Cat("Black")).toString());
+        System.out.println((new Cat("White")).toString());
     }
 
     @Override
