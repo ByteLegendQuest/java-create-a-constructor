4e4a4cc941ea0f4b2141697bd2f3811900f15e1e
itqihou <dongzhoudynasty@outlook.com>
2022-11-24 03:56:28 +0000

    Update at 2022-11-24T03:56:28.746660Z

--- src/main/java/com/bytelegend/Cat.java
+++ src/main/java/com/bytelegend/Cat.java
@@ -3,6 +3,10 @@ package com.bytelegend;
 public class Cat {
     public String name;
 
+    public Cat(String name) {
+        this.name = name;
+    }
+
     public static void main(String[] args) {
         System.out.println(new Cat("Black"));
         System.out.println(new Cat("White"));
@@ -10,6 +14,6 @@ public class Cat {
 
     @Override
     public String toString() {
-        return "Cat(" + name + ")";
+        return "Cat(" + this.name + ")";
     }
 }
